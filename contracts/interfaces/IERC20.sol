7474bec9d7dfe06a8ff8179fe25624dfe0207749
geobla <geomav07@gmail.com>
2022-09-07 03:06:34 +0300

    updates

--- contracts/interfaces/IERC20.sol
+++ contracts/interfaces/IERC20.sol
@@ -1,6 +1,6 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity >=0.6.2 <0.8.0;
+pragma solidity >=0.6.2 <0.9.0;
 
 interface IERC20 {
     function transfer(address recipient, uint256 amount) external returns (bool);
